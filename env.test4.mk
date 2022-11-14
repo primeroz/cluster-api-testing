e2f33a411a36b182aa3dbdfe949990390c80b848
Francesco Ciocchetti <primeroznl@gmail.com>
2022-11-14 15:46:31 +0100

    Add test4

---
+++ env.test4.mk
@@ -0,0 +1,16 @@
+
+CLUSTER_NAME = test4
+AZURE_CLUSTER_IDENTITY_SECRET_NAME = cluster-identity-secret-$(CLUSTER_NAME)
+KUBERNETES_VERSION=1.25
+KUBERNETES_PODS_CIDR_BLOCK=10.244.0.0/16
+KUBERNETES_SERVICES_CIDR_BLOCK=10.100.0.0/16
+
+# Applications to deploy, order is important
+APPS = \
+  azure-resource-group \
+  clusterctl \
+  azure-cluster \
+  azure-cloud-provider \
+  azure-core-apps \
+  azure-cluster-api-autoscaler
+
